1f984a150326770cf05f377f365148c87718fbef
Mike Ireland <michael.ireland@anu.edu.au>
2022-04-08 10:25:57 +1000

    Added double precision.

--- test_rv_fit.py
+++ test_rv_fit.py
@@ -7,7 +7,7 @@ save_dir = './' #'/home/ehold13/veloce_scripts/obs_corrected_fits/'
     #s,w,se = create_observation_fits('11dec30096o.fits',file_name,'/home/ehold13/veloce_scripts/obs_corrected_fits/')
 #s,w,se = create_observation_fits('11dec30096o.fits','14dec30068o.fits','/home/ehold13/veloce_scripts/obs_corrected_fits/')
 fitting_files = ['11dec30096','11dec30097','12dec30132','12dec30133','12dec30134', '13dec30076','13dec30077','14dec30066','14dec30067','14dec30068','15dec30097', '15dec30098', '15dec30099']
-fitting_files = fitting_files[:2] #!!!
+fitting_files = fitting_files[:1] #!!!
 velocity_err = np.zeros([len(fitting_files),36])
 file_ind = 0
 for fit in fitting_files:
@@ -52,22 +52,27 @@ for fit in fitting_files:
         temp_dlwave = temp_lwave[1]-temp_lwave[0]
         rvs = []
         rv_errs = []
+        mses = []
         for j in range(19):
-            spect_mask = np.isnan(spect[501:3350,i,j])
-            spect_wave = wavelength[501:3350,i,j][~spect_mask]
-            spect_spec = spect[501:3350,i,j][~spect_mask]
-            spect_err_ = spect_err[501:3350,i,j][~spect_mask]
+            spect_mask = np.isnan(spect[601:3350,i,j])
+            spect_wave = wavelength[601:3350,i,j][~spect_mask].astype(np.float64) #Converting to float64 is essential!
+            spect_spec = spect[601:3350,i,j][~spect_mask]
+            spect_err_ = np.sqrt(spect_err[601:3350,i,j][~spect_mask]) #??? It seems that the errors are too small.
             spect_lwave = np.log(spect_wave)
             
             # changing intial conditions changes answer and MSE 
-            a = optimise.leastsq(rv_fitting_eqn_old,x0 = [-24,1e-3,1e-3,1e-3], args=(spect_wave, spect_spec, spect_err_, temp_func), epsfcn=1e-6, full_output = True, ftol=1e-6, gtol=1e-6)
+            #a = optimise.leastsq(rv_fitting_eqn_old,x0 = [-24,1e-3,1e-3,1e-3], args=(spect_wave, spect_spec, spect_err_, temp_func), epsfcn=1e-6, full_output = True, ftol=1e-6, gtol=1e-6)
             
 #rv_fitting_eqn(params, lwave, spect, spect_err, template, lwave0, dlwave, return_spec = False):
-            #a = optimise.leastsq(rv_fitting_eqn,x0 = [-22,1e-3,1e-3,1e-3], args=(spect_lwave, spect_spec, spect_err_, temp_spec, temp_lwave[0], temp_dlwave), \
-             #   epsfcn=1e-4, full_output = True, ftol=1e-7, gtol=1e-7)      
+            #a = optimise.leastsq(rv_fitting_eqn,x0 = [-25,1e-3,1e-3,1e-3], args=(spect_lwave, spect_spec, spect_err_, temp_spec, temp_lwave[0], temp_dlwave), \
+            #   epsfcn=1e-4, full_output = True, ftol=1e-7, gtol=1e-7)     
+            a = optimise.least_squares(rv_fitting_eqn,x0 = [-26,0,0,0], args=(spect_lwave, spect_spec, spect_err_, temp_spec, temp_lwave[0], temp_dlwave), \
+                jac=rv_jac, method='lm', ftol=1e-10, xtol=1e-10) 
 
-            print(a[0])
-            
+            print(a.x)
+            print(a.status)
+            cov = np.linalg.inv(np.dot(a.jac.T,a.jac))
+
             if False:
                 plt.figure()
                 plt.plot(spect_wave,a[2]['fvec'])
@@ -75,10 +80,19 @@ for fit in fitting_files:
             #import pdb; pdb.set_trace()
 
             #!!! To be neatened. All velocities should be saved in a 2D array !!!
-            if a[0][0] != -24 and not (a[1] is None):
-               rvs.append(a[0][0])
+            if a.success: #a[0][0] != -24 and not (a[1] is None):
+               rvs.append(a.x[0])
+               mse = np.mean(a.fun**2) #np.mean(a[2]['fvec']**2)
+               mses.append(mse)
                #Multiply the error but the square root of chi-squared. !!! Why is chi-squared so high? !!!
-               rv_errs.append(np.sqrt(np.mean(a[2]['fvec']**2))*np.sqrt(a[1][0,0]))
+               rv_errs.append(np.sqrt(mse)*np.sqrt(cov[0,0]))
+               #Now make a plot to see if the minimum is unique!
+               if True:
+                   rv_test = np.linspace(-24.1,-23.9,201)
+                   this_chi2 = test_rv_chi2(a.x, rv_test, spect_lwave, spect_spec, spect_err_, temp_spec, temp_lwave[0], temp_dlwave)
+                   plt.figure(j)
+                   plt.clf()
+                   plt.plot(rv_test, this_chi2)
 
         rvs = np.array(rvs)
         rv_errs = np.array(rv_errs)
@@ -89,7 +103,8 @@ for fit in fitting_files:
         print("Weighted mean RV (km/s): {:.4f} +/- {:.4f}".format(wtmn_rv, wtmn_rv_err))
         #The mean square error will be significantly larger than 1 (e.g. more than 1.5) if fiber to fiber 
         #variations are determined by more than just random errors.
-        print("MSE: {:.2f}".format(np.mean((wtmn_rv - rvs)**2/rv_errs**2)))
+        print("MSE over fibers: {:.2f}".format(np.mean((wtmn_rv - rvs)**2/rv_errs**2)))
+        print("MSE sum for each fiber: {:.4f}".format(np.sum(mses)))
         if False:
             plt.figure()
             plt.errorbar(np.arange(len(rvs))+1, rvs, rv_errs, fmt='.')
